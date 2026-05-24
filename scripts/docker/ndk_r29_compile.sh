3512f368e99f22e8bcd2ef5e1cb849061cae78ff
417564088 <417564088@qq.com>
2026-05-24 15:05:53 +0800

    更新工具链

--- scripts/docker/ndk_r29_compile.sh
+++ scripts/docker/ndk_r29_compile.sh
@@ -6,29 +6,30 @@
 
 set -ex
 
-if [ -z "${ANDROID_NDK_ROOT:-}" ]; then
-  echo "ANDROID_NDK_ROOT is required"
-  exit 1
-fi
-
 mkdir -p /data
 cd /data
 
+cp /third-party/omvll-deps-ndk-*/android-llvm-toolchain-r29.tar.gz .
 cp /third-party/omvll-deps-ndk-*/Python-slim.tar.gz .
 cp /third-party/omvll-deps-ndk-*/pybind11.tar.gz .
 cp /third-party/omvll-deps-ndk-*/spdlog-1.10.0-Linux.tar.gz .
 
+tar xzvf android-llvm-toolchain-r29.tar.gz
+tar xzvf android-llvm-toolchain-r29/out.tar.gz -C android-llvm-toolchain-r29
+tar xzvf android-llvm-toolchain-r29/out/stage1-install.tar.gz -C android-llvm-toolchain-r29/out
+tar xzvf android-llvm-toolchain-r29/out/stage2.tar.gz -C android-llvm-toolchain-r29/out
 tar xzvf Python-slim.tar.gz
 tar xzvf pybind11.tar.gz
 tar xzvf spdlog-1.10.0-Linux.tar.gz
 
-export NDK_TOOLCHAIN=${ANDROID_NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64
+export NDK_STAGE1=$(pwd)/android-llvm-toolchain-r29/out/stage1-install
+export NDK_STAGE2=$(pwd)/android-llvm-toolchain-r29/out/stage2
 
 mkdir -p /test-deps/bin
-cp ${NDK_TOOLCHAIN}/bin/clang /test-deps/bin
-cp ${NDK_TOOLCHAIN}/bin/clang++ /test-deps/bin
-if [ -f ${NDK_TOOLCHAIN}/bin/llvm-lit ]; then
-  cp ${NDK_TOOLCHAIN}/bin/llvm-lit /test-deps/bin
+cp ${NDK_STAGE2}/bin/clang /test-deps/bin
+cp ${NDK_STAGE2}/bin/clang++ /test-deps/bin
+if [ -f ${NDK_STAGE2}/bin/llvm-lit ]; then
+  cp ${NDK_STAGE2}/bin/llvm-lit /test-deps/bin
 fi
 
 cd /o-mvll/src
@@ -37,15 +38,15 @@ cd o-mvll-build_ndk_r29
 
 cmake -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_COMPILER=${NDK_TOOLCHAIN}/bin/clang++ \
-      -DCMAKE_C_COMPILER=${NDK_TOOLCHAIN}/bin/clang \
+      -DCMAKE_CXX_COMPILER=${NDK_STAGE1}/bin/clang++ \
+      -DCMAKE_C_COMPILER=${NDK_STAGE1}/bin/clang \
       -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
       -DPython3_ROOT_DIR=/data/Python-slim \
       -DPython3_LIBRARY=/data/Python-slim/lib/libpython3.10.a \
       -DPython3_INCLUDE_DIR=/data/Python-slim/include/python3.10 \
       -Dpybind11_DIR=/data/pybind11/share/cmake/pybind11 \
       -Dspdlog_DIR=/data/spdlog-1.10.0-Linux/lib/cmake/spdlog \
-      -DLLVM_DIR=${NDK_TOOLCHAIN}/lib/cmake/llvm \
+      -DLLVM_DIR=${NDK_STAGE2}/lib/cmake/llvm \
       -DLLVM_TOOLS_DIR=/test-deps \
       -DLLVM_REQUIRED_VERSION=21 \
       -DOMVLL_ABI=CustomAndroid
